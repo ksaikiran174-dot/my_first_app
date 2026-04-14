1ef45a7b7d2dbd333a907b5509807a43934e52ad
Sai Kiran <ksaikiran174@gmail.com>
2026-04-14 23:16:18 +0530

    migrated to progres

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
+import os
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL =  "sqlite:///./test.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL , connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
